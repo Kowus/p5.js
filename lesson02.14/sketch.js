df7cb6aa1c64188f515674149013bc644b047ed0
Kowus <barnabasnomo@gmail.com>
2016-09-09 23:11:39 +0000

    Paint with Grays

---
+++ lesson02.14/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+	createCanvas(480, 120);
+	background(0);
+}
+
+function draw() {
+  	fill(204);
+  	ellipse(132, 82, 200, 200);
+  	fill(153);
+  	ellipse(228, -16, 200, 200);
+  	fill(102);
+  	ellipse(268, 118, 200, 200);
+}
