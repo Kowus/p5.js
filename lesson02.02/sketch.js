bfee721114ea9e140afb707de9af6b2bd35d4365
Kowus <barnabasnomo@gmail.com>
2016-09-09 22:44:33 +0000

    Draw a Point

---
+++ lesson02.02/sketch.js
@@ -0,0 +1,30 @@
+/*
+var myInt = 0;
+var myChange = 1;
+*/
+function setup() {
+	createCanvas(480, 120);
+	
+}
+
+function draw() {
+	// Code Example
+	background(204);
+	point(240, 60);
+
+	// Completed Assignment
+	point(0, 0);
+	point(0, 119);
+	point(479, 0);
+	point(479, 119);
+
+	/*
+	point(myInt, 60);
+	
+	ellipse(myInt, 60, 20, 20);
+
+	myInt += myChange;
+
+	if (myInt>=480 || myInt <= 0) myChange = -myChange;
+	*/
+}
