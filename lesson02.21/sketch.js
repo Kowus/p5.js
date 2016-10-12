c7ea8b6b1677abc3d253aec539c267eb266aaff8
Kowus <barnabasnomo@gmail.com>
2016-10-12 04:35:22 +0000

    Robot 1: Draw

--- lesson02.21/sketch.js
+++ lesson02.21/sketch.js
@@ -1,7 +1,31 @@
 function setup() {
+    createCanvas(720, 480);
+    strokeWeight(2);
+    ellipseMode(RADIUS);
 
 }
 
 function draw() {
+    background(204);                        // set background to grey
+
+    // Neck
+    stroke(102);                            // set stroke to grey
+    line(266, 257, 266, 162);               // left
+    line(276, 257, 276, 162);               // middle
+    line(286, 257, 286, 162);               // right
+
+    // Antennae
+    line(276, 155, 246, 112);               // small
+    line(276, 155, 306, 56);                // tall
+    line(276, 155, 342, 170);               // medium
+
+    // Body
+    noStroke();                             // disable stroke
+    fill(102);                              // set fill to grey
+    ellipse(264, 377, 33, 33);              // wheel/anti-gravity orb
+    fill(0);                                // set fill to black
+    rect(219, 257, 90, 120);                // draw rectangle for body
+    fill(102);
+    rect(219, 274, 90, 6);
   
 }
